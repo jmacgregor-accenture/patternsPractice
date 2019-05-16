ebea775a64b83c1b638cb571cc1d35cc3b956969
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-16 16:25:56 -0400

    Gilded Rose ready to factory-ize...

--- GildedFactory/GildedRose.cs
+++ GildedFactory/GildedRose.cs
@@ -25,6 +25,8 @@ namespace csharpcore
                     return new LegendaryItem(name, quality, sellin);
                 case "Backstage passes to a TAFKAL80ETC concert":
                     return new BackStagePassQualityItem(name, quality, sellin);
+                case "Aged Brie":
+                    return new AgedBrieItem(name, quality, sellin);
                 default:
                     return new Item(name, quality, sellin);
             }
@@ -34,57 +36,7 @@ namespace csharpcore
         {
             foreach (var item in Items)
             {
-                if (item.Name == "Aged Brie")
-                {
-                    HandleBrie(item);
-                    continue;
-                }
-                
                 item.Update();
-            }
-            
-            
-        }
-
-        private void HandleBrie(Item item)
-        {
-            IncreaseQuality(item);
-
-            item.SellIn--;
-
-            if (item.SellIn < 0)
-            {
-                IncreaseQuality(item);
-            }
-        }
-
-        private void HandleBackStagePass(Item item)
-        {
-            IncreaseQuality(item);
-
-            item.SellIn--;
-
-            if (item.SellIn < 10)
-            {
-                IncreaseQuality(item);
-            }
-
-            if (item.SellIn < 5)
-            {
-                IncreaseQuality(item);
-            }
-
-            if (item.SellIn < 0)
-            {
-                item.Quality = 0;
-            }
-        }
-
-        private void IncreaseQuality(Item item)
-        {
-            if (item.Quality < 50)
-            {
-                item.Quality += 1;
             }
         }
     }
--- GildedFactory/Items/AgedBrieItem.cs
+++ GildedFactory/Items/AgedBrieItem.cs
@@ -1,7 +1,22 @@
 namespace csharpcore.Items
 {
-    public class AgedBrieItem
+    public class AgedBrieItem : IncreasingQualityItem
     {
-        
+        public override void Update()
+        {
+            IncreaseQuality();
+
+            SellIn--;
+
+            if (SellIn < 0)
+            {
+                IncreaseQuality();
+            }
+        }
+
+        public AgedBrieItem(string itemName, int itemQuality, int itemSellIn) : base(itemName, itemQuality, itemSellIn)
+        {
+            
+        }
     }
 }
